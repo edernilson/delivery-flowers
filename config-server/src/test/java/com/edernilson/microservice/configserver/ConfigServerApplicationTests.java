d9a47f68f6d775cd03435635bac0fce6269d2a5d
Eder Nilson <eder.nilson@pathfind.com.br>
2022-04-23 21:06:27 -0300

    Criado microservice ConfigServer

---
+++ config-server/src/test/java/com/edernilson/microservice/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edernilson.microservice.configserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
